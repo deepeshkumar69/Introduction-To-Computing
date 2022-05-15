8ca8a75be43210376c4751f2f406fb654bd6b10a
deepeshkumar69 <104159122+deepeshkumar69@users.noreply.github.com>
2022-05-15 22:16:14 +0530

    Add files via upload

---
+++ assignment2sol.py
@@ -0,0 +1,39 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/DEEPESH KUMAR/Desktop/assignment2.py ============
+QUESTION 1.)
+PART A
+35
+PART B
+egaugnal evitisnes esac a si nohtyP
+PART C
+SLICED STRING IS:  a case sensitive
+PART D
+Python is object oriented language
+PART E
+10
+PART F
+Pythonisacasesensitivelanguage
+QUESTION 2.)
+Hey,Deepesh Kumar Here!
+My SID is 21102121
+I am from CIVIL department and my CGPA is 9.9
+QUESTION 3.)
+a&b is:  8
+a|b is:  58
+a^b is:  50
+a<<2: 224 	b<<2:  40
+a>>2: 14 	b>>4:  0
+QUESTION 4.)
+ENTER ANY STRING : my name is deepesh
+YES
+QUESTION 5.)
+ENTER LENGTH OF FIRST SIDE OF TRIANGLE: 10
+ENTER LENGTH OF SECOND SIDE OF TRIANGLE: 20
+ENTER LENGTH OF THIRD SIDE OF TRIANGLE: 17
+YES
+QUESTION 6.)
+enter a number : 20
+enter another number : 30
+2
